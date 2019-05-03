2ce36ed6252a6fd7546178eb1ebecca65a59a3b8
William Wood <wcw838@uowmail.edu.au>
2019-05-04 08:54:48 +1000

    should now get active services in list

--- app/src/main/java/com/example/roadsideassistance/roadsideSelectActiveService.java
+++ app/src/main/java/com/example/roadsideassistance/roadsideSelectActiveService.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -21,6 +22,29 @@ public class roadsideSelectActiveService extends AppCompatActivity {
         activeServices = roadsideAssistant.getActiveServices();
 
         final LinearLayout activeServicesLayout = findViewById(R.id.roadsideActiveSelectLayout);
+        if (activeServices != null && activeServices.size() > 0) {
+            for (int i = 0; i < activeServices.size(); i++) {
+                final int currIndex = i;
+                final TextView serviceText = new TextView(this);
+                serviceText.setText("Customer: " + activeServices.get(i).customer_username + " Plate: " + activeServices.get(i).car_plateNum + " Cost: " + activeServices.get(i).cost);
+                serviceText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectedActiveServiceIndex = currIndex;
+                        for (int j = 0; j < activeServicesLayout.getChildCount(); j++) {
+                            activeServicesLayout.getChildAt(j).setBackgroundColor(getResources().getColor(R.color.unselectedListItem));
+                        }
+                        serviceText.setBackgroundColor(getResources().getColor(R.color.selectedListItem));
+                    }
+                });
+                activeServicesLayout.addView(serviceText);
+            }
+        }
+        else {
+            TextView noActiveServices = new TextView(this);
+            noActiveServices.setText("No Active Services");
+            activeServicesLayout.addView(noActiveServices);
+        }
     }
 
     public void roadsideSelectActiveService(View view) {
