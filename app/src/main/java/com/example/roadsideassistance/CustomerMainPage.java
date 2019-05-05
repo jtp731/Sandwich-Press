ed4a47bf385477f05735849d0b5e5e4860e745d7
jtp731 <jtp731@uowmail.edu.au>
2019-05-05 12:48:34 +1000

    Something is happening?

---
+++ app/src/main/java/com/example/roadsideassistance/CustomerMainPage.java
@@ -0,0 +1,13 @@
+package com.example.roadsideassistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main_page);
+    }
+}
