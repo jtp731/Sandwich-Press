dab5c57c3604a42327de2f8f43a245dedf2a721c
ajlambo1995 <Toyota#85733>
2019-05-07 17:57:42 +1000

    Addition of some Assistant GUI

---
+++ app/src/main/java/com/example/roadsideassistance/AssistantAccountSettings.java
@@ -0,0 +1,13 @@
+package com.example.roadsideassistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssistantAccountSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assistant_account_settings);
+    }
+}
---
+++ app/src/main/java/com/example/roadsideassistance/AssistantDetails.java
@@ -0,0 +1,13 @@
+package com.example.roadsideassistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssistantDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assistant_details);
+    }
+}
