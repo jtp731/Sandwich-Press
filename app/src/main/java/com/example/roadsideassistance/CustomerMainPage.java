3cfc351feb7d04cf403665f03461285234171a7b
William Wood <wcw838@uowmail.edu.au>
2019-05-07 19:31:30 +1000

    Added main pages

---
+++ app/src/main/java/com/example/roadsideassistance/CustomerMainPage.java
@@ -0,0 +1,48 @@
+package com.example.roadsideassistance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class CustomerMainPage extends AppCompatActivity {
+    Customer customer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main_page);
+
+        customer = getIntent().getParcelableExtra("Customer");
+
+        //Toast.makeText(this, "" + roadsideAssistant.services.size(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void toNewService(View view) {
+        Intent intent = new Intent(this, CustomerServiceRequest.class);
+        intent.putExtra("Customer", customer);
+        startActivityForResult(intent, 1);
+
+        Toast.makeText(this, "" + customer.services.size(), Toast.LENGTH_LONG).show();
+    }
+
+    public void toCustServiceOffers(View view) {
+        Intent intent = new Intent(this, CustomerServiceActive.class);
+        intent.putExtra("Customer", customer);
+        startActivityForResult(intent, 1);
+    }
+
+    public void toFinishedServices(View view) {
+        Intent intent = new Intent(this, CustomerServiceFinishedSelect.class);
+        intent.putExtra("Customer", customer);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == 1) {
+                customer = data.getParcelableExtra("Customer");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/roadsideassistance/RoadsideMainPage.java
@@ -0,0 +1,46 @@
+package com.example.roadsideassistance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class RoadsideMainPage extends AppCompatActivity {
+    RoadsideAssistant roadsideAssistant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roadside_main_page);
+
+        roadsideAssistant = getIntent().getParcelableExtra("RoadsideAssistant");
+
+        //Toast.makeText(this, "" + roadsideAssistant.services.size(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void toNewServices(View view) {
+        Intent intent = new Intent(this, RoadsideServiceSelect.class);
+        intent.putExtra("Roadside", roadsideAssistant);
+        startActivityForResult(intent, 1);
+    }
+
+    public void toServiceOffers(View view) {
+        Intent intent = new Intent(this, RoadsideSelectOffer.class);
+        intent.putExtra("Roadside", roadsideAssistant);
+        startActivityForResult(intent, 1);
+    }
+
+    public void toAcceptedServices(View view) {
+        Intent intent = new Intent(this, RoadsideSelectActiveService.class);
+        intent.putExtra("Roadside", roadsideAssistant);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == 1) {
+            roadsideAssistant = data.getParcelableExtra("Roadside");
+        }
+    }
+}
