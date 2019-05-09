39a152ffe9d4f87ad4104d0f8be024b19ede130d
William Wood <wcw838@uowmail.edu.au>
2019-05-09 11:14:53 +1000

    Added roadside signup credentials

--- app/src/main/java/com/example/roadsideassistance/registerRoadside.java
+++ app/src/main/java/com/example/roadsideassistance/registerRoadside.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class registerRoadside extends AppCompatActivity {
     boolean canTow = false;
@@ -20,6 +24,10 @@ public class registerRoadside extends AppCompatActivity {
         Intent intent = getIntent();
         person = intent.getParcelableExtra("Person");
         database = AppDatabase.getDatabase(getApplicationContext());
+
+        findViewById(R.id.licenceError).setVisibility(View.GONE);
+        findViewById(R.id.companyNameError).setVisibility(View.GONE);
+        findViewById(R.id.ABNError).setVisibility(View.GONE);
 
         Switch towable = findViewById(R.id.towToggle);
         towable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -36,8 +44,69 @@ public class registerRoadside extends AppCompatActivity {
     }
 
     public void finishRegister(View view) {
-        RoadsideAssistant roadsideAssistant = new RoadsideAssistant(person, canTow);
-        database.userDao().addRoadsideAssistant(roadsideAssistant);
-        finish();
+        if(verify()) {
+            TextView input = findViewById(R.id.newLicenceNumber);
+            String licence = input.getText().toString().trim();
+
+            input = findViewById(R.id.newCompanyName);
+            String companyName = input.getText().toString().trim();
+
+            input = findViewById(R.id.newABN);
+            String abnStr = input.getText().toString().trim();
+            long abn = Long.parseLong(abnStr);
+
+            RoadsideAssistant roadsideAssistant = new RoadsideAssistant(person, licence, companyName, abn, canTow, 0.0f);
+            database.userDao().addRoadsideAssistant(roadsideAssistant);
+            finish();
+        }
+    }
+
+    boolean verify() {
+        boolean good = true;
+
+
+        TextView licenceError = findViewById(R.id.licenceError);
+        TextView companyError = findViewById(R.id.companyNameError);
+        TextView abnError = findViewById(R.id.ABNError);
+
+        licenceError.setVisibility(View.GONE);
+        companyError.setVisibility(View.GONE);
+        abnError.setVisibility(View.GONE);
+
+        String licencePattern = "^MV(TC)|(RL)[0-9]{5}$";
+        String abnPattern = "^[0-9]{11}$";
+
+        TextView input = findViewById(R.id.newLicenceNumber);
+        String licence = input.getText().toString().trim();
+
+        input = findViewById(R.id.newCompanyName);
+        String companyName = input.getText().toString().trim();
+
+        input = findViewById(R.id.newABN);
+        String abnStr = input.getText().toString().trim();
+
+        Pattern pattern = Pattern.compile(licencePattern);
+        Matcher matcher = pattern.matcher(licence);
+        if(!matcher.matches()) {
+            good = false;
+            licenceError.setText("Licence number must be in MVTC12345 or MVRL12345 format");
+            licenceError.setVisibility(View.VISIBLE);
+        }
+
+        if(companyName.equals("")) {
+            good = false;
+            companyError.setText("Company Name must be filled");
+            companyError.setVisibility(View.VISIBLE);
+        }
+
+        pattern = Pattern.compile(abnPattern);
+        matcher = pattern.matcher(abnStr);
+        if(!matcher.matches()) {
+            good = false;
+            abnError.setText("ABN is 11 digits");
+            abnError.setVisibility(View.VISIBLE);
+        }
+
+        return good;
     }
 }
