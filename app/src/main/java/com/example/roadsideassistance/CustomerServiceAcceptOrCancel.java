98177ec64de2f29b105b0d963c5e57397e74a40b
William Wood <wcw838@uowmail.edu.au>
2019-05-12 14:42:33 +1000

    made better offer gui

--- app/src/main/java/com/example/roadsideassistance/CustomerServiceAcceptOrCancel.java
+++ app/src/main/java/com/example/roadsideassistance/CustomerServiceAcceptOrCancel.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,11 +31,72 @@ public class CustomerServiceAcceptOrCancel extends AppCompatActivity {
         TextView serviceDescription = findViewById(R.id.customerSelectedServiceDescription);
         serviceDescription.setText(activeService.toString());
 
+        View rating = findViewById(R.id.rating);
+        rating.post(new Runnable() {
+            @Override
+            public void run() {
+                createOfferList();
+            }
+        });
+    }
+
+    private void createOfferList() {
         final LinearLayout serviceOffersLayout = findViewById(R.id.customerServiceOffersLayout);
         boolean coveredBySubscription = customer.carCoveredBySubscription(activeService.car_plateNum);
         if(serviceOffers.size() > 0) {
             for(int i = 0; i < serviceOffers.size(); i++) {
                 final int currIndex = i;
+                final LinearLayout offerLayout = new LinearLayout(this);
+                offerLayout.setOrientation(LinearLayout.HORIZONTAL);
+                offerLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                RoadsideAssistant offerer = database.roadsideAssistantDao().getRoadsideAssistantByUsername(serviceOffers.get(i).roadside_assistant_username);
+
+                TextView usernameText = new TextView(this);
+                usernameText.setPadding(5,5,5,5);
+                usernameText.setWidth(findViewById(R.id.roadsideUsername).getWidth());
+                usernameText.setBackground(getResources().getDrawable(R.drawable.border_sharp));
+                usernameText.setText(offerer.username);
+                offerLayout.addView(usernameText);
+
+                TextView costText = new TextView(this);
+                costText.setPadding(5,5,5,5);
+                costText.setWidth(findViewById(R.id.cost).getWidth());
+                costText.setBackground(getResources().getDrawable(R.drawable.border_sharp));
+                if(!coveredBySubscription)
+                    costText.setText(String.format("$%.2f", serviceOffers.get(i).cost));
+                else
+                    costText.setText("Free");
+                offerLayout.addView(costText);
+
+                LinearLayout ratingBarLayout = new LinearLayout(this);
+                ratingBarLayout.setPadding(5, 5, 5, 5);
+                ratingBarLayout.setLayoutParams(new LinearLayout.LayoutParams(findViewById(R.id.rating).getWidth(), ViewGroup.LayoutParams.MATCH_PARENT));
+                RatingBar ratingBar = new RatingBar(this, null, android.R.attr.ratingBarStyleSmall);
+                ratingBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                ratingBar.setNumStars(5);
+                ratingBar.setStepSize(0.5f);
+                ratingBar.setIsIndicator(true);
+                ratingBar.setRating(offerer.rating);
+                //ratingBar.setPadding(5,5,5,5);
+                //ratingBar.setScaleX(0.5f);
+                //ratingBar.setScaleY(0.5f);
+                ratingBarLayout.setBackground(getResources().getDrawable(R.drawable.border_sharp));
+                ratingBarLayout.addView(ratingBar);
+                offerLayout.addView(ratingBarLayout);
+
+                offerLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectedOfferIndex = currIndex;
+                        for(int j = 0; j < serviceOffersLayout.getChildCount(); j++) {
+                            serviceOffersLayout.getChildAt(j).setBackgroundColor(getResources().getColor(R.color.unselectedListItem));
+                        }
+                        offerLayout.setBackgroundColor(getResources().getColor(R.color.selectedListItem));
+                    }
+                });
+                serviceOffersLayout.addView(offerLayout);
+                /*
                 final TextView serviceOffer = new TextView(this);
                 RoadsideAssistant offerer = database.roadsideAssistantDao().getRoadsideAssistantByUsername(serviceOffers.get(i).roadside_assistant_username);
                 String text = "Username: " + offerer.username + " Rating: " + offerer.rating;
@@ -51,6 +114,7 @@ public class CustomerServiceAcceptOrCancel extends AppCompatActivity {
                     }
                 });
                 serviceOffersLayout.addView(serviceOffer);
+                */
             }
         }
         else {
