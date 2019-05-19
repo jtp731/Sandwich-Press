349fddd28036ef0fe34f3cebe049c1589ab56adb
William Wood <wcw838@uowmail.edu.au>
2019-05-19 10:59:09 +1000

    added cust pay history class and layout file

---
+++ app/src/main/java/com/example/roadsideassistance/CustomerPaymentHistory.java
@@ -0,0 +1,14 @@
+package com.example.roadsideassistance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CustomerPaymentHistory extends AppCompatActivity {
+    Customer customer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_payment_history);
+    }
+}
