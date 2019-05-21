89ff937dc2b00818b3df5a48a26f0879d8f74aaf
ajlambo1995 <Toyota#85733>
2019-05-22 09:37:41 +1000

    new roadside assistant menu

--- app/src/main/java/com/example/roadsideassistance/AssistantDetails.java
+++ app/src/main/java/com/example/roadsideassistance/AssistantDetails.java
@@ -1,13 +1,29 @@
 package com.example.roadsideassistance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AssistantDetails extends AppCompatActivity {
+
+    RoadsideAssistant roadsideAssistant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assistant_details);
+
+        roadsideAssistant = getIntent().getParcelableExtra("RoadsideAssistant");
+
+        Button buttonChangePassword = findViewById(R.id.buttonChangePassword);
+        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AssistantDetails.this, RoadsideChangePassword.class);
+                intent.putExtra("Roadside", roadsideAssistant);
+                startActivity(intent);
+            }
+        });
     }
 }
