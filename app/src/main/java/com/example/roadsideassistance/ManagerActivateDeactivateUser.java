0fce81c896c462766ebd31678870a0223d1af3ba
aam509 <49514606+aam509@users.noreply.github.com>
2019-06-04 12:46:58 +1000

    commit manager to main

--- app/src/main/java/com/example/roadsideassistance/ManagerActivateDeactivateUser.java
+++ app/src/main/java/com/example/roadsideassistance/ManagerActivateDeactivateUser.java
@@ -1,14 +1,23 @@
 package com.example.roadsideassistance;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
 
 
 public class ManagerActivateDeactivateUser extends AppCompatActivity {
     Manager manager;
     AppDatabase database;
+    EditText firstName, lastName;
+    Button searchButton, activateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,26 @@ public class ManagerActivateDeactivateUser extends AppCompatActivity {
         database = AppDatabase.getDatabase(getApplicationContext());
 
         manager = getIntent().getParcelableExtra("Manager");
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+
+        searchButton = findViewById(R.id.searchButton);
+        activateButton  = findViewById(R.id.activateButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        activateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
