37d52b2e85209a9bbf65d8ce16b015d16b5bdd90
aam509 <49514606+aam509@users.noreply.github.com>
2019-06-05 12:28:44 +1000

    changes mande to manage activatedeactivate user

--- app/src/main/java/com/example/roadsideassistance/ManagerActivateDeactivateUser.java
+++ app/src/main/java/com/example/roadsideassistance/ManagerActivateDeactivateUser.java
@@ -4,10 +4,12 @@ package com.example.roadsideassistance;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.database.Cursor;
 import android.app.Activity;
 import android.content.Context;
@@ -29,7 +31,11 @@ public class ManagerActivateDeactivateUser extends AppCompatActivity {
     AppDatabase database;
     String sfirstName, slastName;
     EditText firstName, lastName;
-    Button searchButton, activateButton;
+    Button searchButton, activateButton, deactivateButton;
+    TextView textView;
+    //Boolean check = true;
+
+    LinearLayout userListLayout,userListLayout2,userListLayout3,userListLayout4,userListLayout5,userListLayout6, userListLayout7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,59 +51,80 @@ public class ManagerActivateDeactivateUser extends AppCompatActivity {
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
 
+
         searchButton = findViewById(R.id.searchButton);
         activateButton  = findViewById(R.id.activateButton);
+        deactivateButton = findViewById(R.id.deactivateButton);
+        userListLayout = findViewById(R.id.userListLayout);
+        userListLayout2 = findViewById(R.id.userListLayout2);
+        userListLayout5 = findViewById(R.id.userListLayout5);
+        //userListLayout3 = findViewById(R.id.userListLayout3);
+        //userListLayout4 = findViewById(R.id.userListLayout4);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sfirstName = firstName.getText().toString();
-                slastName = lastName.getText().toString();
+        //userListLayout6 = findViewById(R.id.userListLayout6);
+        //userListLayout7 = findViewById(R.id.userListLayout7);
 
-            }
-        });
+        searchButton.setOnClickListener
+                (
+                        new View.OnClickListener()
+                        {
+                            public void onClick(View v)
+                            {
+                                if (userListLayout.getVisibility() == View.VISIBLE)
+                                {
+                                    userListLayout.setVisibility(View.INVISIBLE);
 
-        activateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                                    if(firstName.getText().toString().contains("cust35")){
+                                        userListLayout2.setVisibility(View.VISIBLE);
+                                        activateButton.setOnClickListener
+                                                (
+                                                        new View.OnClickListener()
+                                                        {
+                                                            public void onClick(View v)
+                                                            {
+                                                                userListLayout2.setVisibility(View.VISIBLE);
+                                                                userListLayout5.setVisibility(View.INVISIBLE);
 
-            }
-        });
+                                                            }
+                                                        }
+                                                );
+                                        deactivateButton.setOnClickListener
+                                                (
+                                                        new View.OnClickListener()
+                                                        {
+                                                            public void onClick(View v)
+                                                            {
+                                                                userListLayout2.setVisibility(View.INVISIBLE);
+                                                                userListLayout5.setVisibility(View.VISIBLE);
+
+                                                            }
+                                                        }
+                                                );
+                                        Log.d("Cus", "Is working");
+                                    }
+
+                                }
+                                else
+                                {
+                                    userListLayout.setVisibility(View.VISIBLE);
+                                    userListLayout2.setVisibility(View.INVISIBLE);
+                                    //userListLayout3.setVisibility(View.INVISIBLE);
+                                    //userListLayout4.setVisibility(View.INVISIBLE);
+                                    userListLayout5.setVisibility(View.INVISIBLE);
+                                    //userListLayout6.setVisibility(View.INVISIBLE);
+                                    //userListLayout7.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        }
+                );
+
+
 
     }
 
-    public class TodoCursorAdapter extends CursorAdapter {
-        public TodoCursorAdapter(Context context, Cursor cursor) {
-            super(context, cursor, 0);
-        }
 
-        // The newView method is used to inflate a new view and return it,
-        // you don't bind any data to the view at this point.
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
-            return LayoutInflater.from(context).inflate(R.layout.item_todo, parent, false);
-        }
 
-        // The bindView method is used to bind all data to a given view
-        // such as setting the text on a TextView.
-        @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            // Find fields to populate in inflated template
-            TextView tvUserType = (TextView) view.findViewById(R.id.userType);
-            TextView tvName = (TextView) view.findViewById(R.id.name);
-            TextView tvStatus = (TextView) view.findViewById(R.id.status);
-            // Extract properties from cursor
-            String userType = cursor.getString(cursor.getColumnIndexOrThrow("userType"));
-            String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
-            String status = cursor.getString(cursor.getColumnIndexOrThrow("status"));
 
-            // Populate fields with extracted properties
-            tvUserType.setText(userType);
-            tvName.setText(name);
-            tvStatus.setText(status);
-
-        }
-    }
 
 
 }
